fba332c107420efe92244bd149958b98fcd4ba40
Ganesh Pendyala <ganeshpendyala0314@gmail.com>
2023-11-21 22:52:05 +0530

    first commit

---
+++ password-genrator/src/App.jsx
@@ -0,0 +1,105 @@
+import React from 'react'
+import { useState, useCallback, useEffect, useRef } from 'react'
+
+const App = () => {
+  // State hook.
+  const [length, setLength] = useState(8);
+  const [numberAllowed, setNumberAllowed] = useState(true);
+  const [charAllowed, setcharAllowd] = useState(true);
+  const [password, setPassword] = useState('');
+
+  // Ref hook
+  const passwordRef = useRef(null);
+
+  // Callback Hook .
+  const PasswordGenrator = useCallback(() => {
+    let pass = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    if (numberAllowed) str += "0123456789";
+    if (charAllowed) str += "!@#$%^&*-_+=[]{}`~";
+
+    for (let i = 1; i <= length; i++) {
+      let char = Math.floor(Math.random() * str.length + 1);
+      pass += str.charAt(char);
+    }
+
+    setPassword(pass);
+  }, [length, numberAllowed, charAllowed, setPassword])
+
+
+  //Copy to clipboard function
+  const copyPasswordtoClipboard = useCallback(() => {
+    passwordRef.current?.select();
+    window.navigator.clipboard.writeText(password);
+  }, [password])
+
+  // useEffect hook - Arguments taken by useEffect Hook  (callback function , dependency array)
+  useEffect(
+    () => { PasswordGenrator() }, [length, numberAllowed, charAllowed,
+    PasswordGenrator]
+  )
+  return (
+    <>
+      <div className=' w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500'>
+        <h1 className='text-white text-center my-3'>Password generator</h1>
+        <div className='flex shadow rounded-lg overflow-hidden mb-4'>
+          <input type="text"
+            value={password}
+            className='outline-none w-full py-1 px-3'
+            placeholder='Password'
+            readOnly
+            ref={passwordRef}
+          />
+          <button className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0 hover:bg-white
+           hover:text-blue-700  hover:outline-blue-700 hover:duration-200'
+            onClick={copyPasswordtoClipboard}>
+            copy</button>
+        </div>
+        <div className='flex text-sm gap-x-2'>
+
+          {/* Slider */}
+          <div className='flex items-center gap-x-1'>
+            <input type="range"
+              min={8}
+              max={20}
+              value={length}
+              className='cursor-pointer'
+              onChange={(e) => { setLength(e.target.value) }}
+            />
+            <label >Length: {length}</label>
+          </div>
+
+          {/* Number Checkbox. */}
+          <div className="flex items-center gap-x-1">
+            <input
+              type="checkbox"
+              defaultChecked={numberAllowed}
+              id="numberInput"
+              onChange={() => {
+                setNumberAllowed((prev) => !prev);
+              }}
+            />
+            <label htmlFor="numberInput">Numbers</label>
+          </div>
+
+
+          {/* /Character Checkbox */}
+          <div className="flex items-center gap-x-1">
+            <input
+              type="checkbox"
+              defaultChecked={charAllowed}
+              id="characterInput"
+              onChange={() => {
+                setcharAllowd((prev) => !prev)
+              }}
+            />
+            <label htmlFor="characterInput">Characters</label>
+          </div>
+
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
